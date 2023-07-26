1cd007af2175631668456caab17382fe588f5b0c
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-07-26 14:21:52 +0100

    Refactor nested method extensions

--- JsonExtensions/Source/NestedExtensions.cs
+++ JsonExtensions/Source/NestedExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text.Json.Nodes;
 using System.Collections.Generic;
 
@@ -6,21 +7,22 @@ namespace viral32111.JsonExtensions;
 
 public static class NestedExtensions {
 
-	/*
-	/// <summary>
-	/// Sets the value of a nested JSON object property. If the property or parent JSON object does not exist, it will be created.
+	/// <summary>Sets the value of a nested JSON object property. If the property or parent JSON object does not exist, it will be created.</summary>
+	/// <param name="jsonObject">The JSON object.</param>
 	/// <param name="propertyPath">The nested path to the property, separated by <paramref name="propertyDelimiter"/>.</param>
 	/// <param name="newValue">The new value of the property.</param>
 	/// <param name="propertyDelimiter">The character used to separate nested property names.</param>
-	/// </summary>
+	/// <exception cref="ArgumentException">Thrown if the nested property path is null or whitespace.</exception>
+	/// <exception cref="JsonPropertyNullException">Thrown if one of the nested parent JSON object properties is null.</exception>
 	public static void NestedSet( this JsonObject jsonObject, string propertyPath, JsonNode? newValue, char propertyDelimiter = Defaults.NestedPropertyDelimiter ) {
+		if ( string.IsNullOrWhiteSpace( propertyPath ) ) throw new ArgumentException( "Nested property path cannot be null or whitespace", nameof( propertyPath ) );
 
 		// Separate the path into individual property names
 		string[] propertyNames = propertyPath.Split( propertyDelimiter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries );
 
-		// Find the object to set the value of, creating it if it does not exist
+		// Find the object to set the property in...
 		JsonObject targetObject = propertyNames
-			.Take( propertyNames.Length - 1 )
+			.Take( propertyNames.Length - 1 ) // No need to include the last property name as we are using it as the index below
 			.Aggregate( jsonObject, ( currentObject, currentPropertyName ) => {
 				if ( !currentObject.ContainsKey( currentPropertyName ) ) currentObject.Add( currentPropertyName, new JsonObject() );
 
@@ -31,170 +33,105 @@ public static class NestedExtensions {
 		targetObject[ propertyNames.Last() ] = newValue;
 
 	}
-	*/
 
-	// Sets the value of a nested property
-	public static void NestedSet( this JsonObject jsonObject, string propertyPath, JsonNode? newValue ) {
-
-		// Separate the path into individual property names
-		string[] propertyNames = propertyPath.Split( Defaults.NestedPropertyDelimiter );
-
-		// The JSON object to search, starts at the root
-		JsonObject nextObject = jsonObject;
-
-		// Loop through each property name...
-		for ( int index = 0; index < propertyNames.Length; index++ ) {
-			string propertyName = propertyNames[ index ];
-
-			// Set the value of the property with this name if this is the last iteration
-			if ( ( index + 1 ) == propertyNames.Length ) nextObject[ propertyName ] = newValue;
-
-			// If this is not the last iteration...
-			else {
-
-				// Set the property with this name to an empty JSON object if it does not exist
-				if ( !nextObject.ContainsKey( propertyName ) ) nextObject.Add( propertyName, new JsonObject() );
-
-				// Get the value of the property with this name
-				JsonNode? value = nextObject[ propertyName ];
-
-				// Fail if the value is invalid
-				if ( value == null ) throw new JsonPropertyNullException( $"Nested property '{propertyName}' in '{propertyPath}' is null, it cannot be used as the next object" );
-
-				// Set this value as the next JSON object to search
-				nextObject = value.AsObject();
-
-			}
-
-		}
-
-	}
-
-	// Gets the value of a nested property
-	public static JsonNode? NestedGet( this JsonObject jsonObject, string propertyPath, char nestedPropertyDelimiter = Defaults.NestedPropertyDelimiter ) {
+	/// <summary>Gets the value of a nested JSON object property.</summary>
+	/// <param name="jsonObject">The JSON object.</param>
+	/// <param name="propertyPath">The nested path to the property, separated by <paramref name="propertyDelimiter"/>.</param>
+	/// <param name="propertyDelimiter">The character used to separate nested property names.</param>
+	/// <returns>The value of the nested JSON object property.</returns>
+	/// <exception cref="ArgumentException">Thrown if the nested property path is null or whitespace.</exception>
+	/// <exception cref="JsonPropertyNullException">Thrown if one of the nested parent JSON object properties is null.</exception>
+	/// <exception cref="JsonPropertyNotFoundException">Thrown if one of the nested JSON object properties does not exist.</exception>
+	public static JsonNode? NestedGet( this JsonObject jsonObject, string propertyPath, char propertyDelimiter = Defaults.NestedPropertyDelimiter ) {
+		if ( string.IsNullOrWhiteSpace( propertyPath ) ) throw new ArgumentException( "Nested property path cannot be null or whitespace", nameof( propertyPath ) );
 
 		// Separate the path into individual property names
-		string[] propertyNames = propertyPath.Split( nestedPropertyDelimiter );
+		string[] propertyNames = propertyPath.Split( propertyDelimiter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries );
 
-		// The JSON object to search, starts at the root
-		JsonObject nextObject = jsonObject;
+		// Find the object to get the property in...
+		JsonObject targetObject = propertyNames
+			.Take( propertyNames.Length - 1 ) // No need to include the last property name as we are using it as the index below
+			.Aggregate( jsonObject, ( currentObject, currentPropertyName ) => {
+				if ( !currentObject.TryGetPropertyValue( currentPropertyName, out JsonNode? currentPropertyValue ) ) throw new JsonPropertyNotFoundException( $"Nested JSON object property '{ currentPropertyName }' in '{ propertyPath }' does not exist, it cannot be used as the next object" );
+				return currentObject[ currentPropertyName ]?.AsObject() ?? throw new JsonPropertyNullException( $"Nested JSON object property '{ currentPropertyName }' in '{ propertyPath }' is null, it cannot be used as the next object" );
+			} );
 
-		// Loop through each property name...
-		for ( int index = 0; index < propertyNames.Length; index++ ) {
-			string propertyName = propertyNames[ index ];
-
-			// Get the value of the property with this name
-			if ( !nextObject.TryGetPropertyValue( propertyName, out JsonNode? value ) ) throw new JsonPropertyNotFoundException( $"Nested property '{propertyName}' in '{propertyPath}' does not exist" );
-
-			// Return the value if this is the last iteration
-			if ( ( index + 1 ) == propertyNames.Length ) return value;
-
-			// If this is not the last iteration...
-			else {
-
-				// Fail if the value is invalid
-				if ( value == null ) throw new JsonPropertyNullException( $"Nested property '{propertyName}' in '{propertyPath}' is null, it cannot be used as the next object" );
-
-				// Set this value as the next JSON object to search
-				nextObject = value.AsObject();
-
-			}
-
-		}
-
-		// Fail if the loop never ran
-		throw new JsonPropertyNotFoundException( $"Could not find nested property '{propertyPath}'" );
+		// Return the value of the property in the found object, or fail
+		if ( !targetObject.TryGetPropertyValue( propertyNames.Last(), out JsonNode? propertyValue ) ) throw new JsonPropertyNotFoundException( $"Nested JSON object property '{ propertyNames.Last() }' from '{ propertyPath }' in '{ jsonObject }' does not exist" );
+		return propertyValue;
 
 	}
 
-	// Gets the value of a nested property, as a certain type
-	public static T NestedGet<T>( this JsonObject jsonObject, string propertyPath ) {
+	/// <summary>Gets the value of a nested JSON object property, casted to the given type.</summary>
+	/// <param name="jsonObject">The JSON object.</param>
+	/// <param name="propertyPath">The nested path to the property, separated by <paramref name="propertyDelimiter"/>.</param>
+	/// <param name="propertyDelimiter">The character used to separate nested property names.</param>
+	/// <returns>The value of the nested JSON object property, as the given type.</returns>
+	/// <exception cref="ArgumentException">Thrown if the nested property path is null or whitespace.</exception>
+	/// <exception cref="JsonPropertyNullException">Thrown if the property cannot be casted because it is null.</exception>
+	public static T NestedGet<T>( this JsonObject jsonObject, string propertyPath, char propertyDelimiter = Defaults.NestedPropertyDelimiter ) {
+		if ( string.IsNullOrWhiteSpace( propertyPath ) ) throw new ArgumentException( "Nested property path cannot be null or whitespace", nameof( propertyPath ) );
 
-		// Get the value of the nested property
-		JsonNode? value = jsonObject.NestedGet( propertyPath );
+		// Get the value of the property, or fail if it does not exist
+		JsonNode propertyValue = jsonObject.NestedGet( propertyPath ) ?? throw new JsonPropertyNullException( $"Nested JSON object property '{ propertyPath }' is null, it cannot be casted to type '{ typeof( T ) }'" );
 
-		// Fail if the value is invalid
-		if ( value == null ) throw new JsonPropertyNullException( $"Nested property '{propertyPath}' is null, it cannot be of type {typeof( T )}" );
-
-		// Return the value as a JSON object
-		if ( typeof( T ) == typeof( JsonObject ) ) return ( T ) ( object ) value.AsObject();
-
-		// Return the value as a JSON array
-		else if ( typeof( T ) == typeof( JsonArray ) ) return ( T ) ( object ) value.AsArray();
-
-		// Return the value as the desired type
-		else return value.GetValue<T>();
+		// Return as JSON object/array, or generic - Switch statement does not work for typeof()!
+		if ( typeof( T ) == typeof( JsonObject ) ) return ( T ) ( object ) propertyValue.AsObject();
+		else if ( typeof( T ) == typeof( JsonArray ) ) return ( T ) ( object ) propertyValue.AsArray();
+		else return propertyValue.GetValue<T>();
 
 	}
 
-	// Checks if a nested property exists
-	public static bool NestedHas( this JsonObject jsonObject, string propertyPath ) {
+	/// <summary>Checks if a nested JSON object property exists.</summary>
+	/// <param name="jsonObject">The JSON object.</param>
+	/// <param name="propertyPath">The nested path to the property, separated by <paramref name="propertyDelimiter"/>.</param>
+	/// <param name="propertyDelimiter">The character used to separate nested property names.</param>
+	/// <returns>If the given property exists.</returns>
+	/// <exception cref="ArgumentException">Thrown if the nested property path is null or whitespace.</exception>
+	/// <exception cref="JsonPropertyNullException">Thrown if the property cannot be casted because it is null.</exception>
+	public static bool NestedHas( this JsonObject jsonObject, string propertyPath, char propertyDelimiter = Defaults.NestedPropertyDelimiter ) {
+		if ( string.IsNullOrWhiteSpace( propertyPath ) ) throw new ArgumentException( "Nested property path cannot be null or whitespace", nameof( propertyPath ) );
 
 		// Separate the path into individual property names
-		string[] propertyNames = propertyPath.Split( Defaults.NestedPropertyDelimiter );
+		string[] propertyNames = propertyPath.Split( propertyDelimiter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries );
 
-		// The JSON object to search, starts at the root
-		JsonObject nextObject = jsonObject;
-
-		// Loop through each property name...
-		for ( int index = 0; index < propertyNames.Length; index++ ) {
-			string propertyName = propertyNames[ index ];
-
-			// Fail if we cannot get the property
-			if ( !nextObject.TryGetPropertyValue( propertyName, out JsonNode? value ) ) return false;
-
-			// If this is not the last iteration...
-			if ( ( index + 1 ) < propertyNames.Length ) {
-
-				// Fail if the value is invalid
-				if ( value == null ) throw new JsonPropertyNullException( $"Nested property '{propertyName}' in '{propertyPath}' is null, it cannot be used as the next object" );
-
-				// Set this value as the next JSON object to search
-				nextObject = value.AsObject();
-
-			}
-
+		// Find the object to check the property in...
+		JsonObject targetObject = jsonObject;
+		foreach ( string propertyName in propertyNames.Take( propertyNames.Length - 1 ) ) {
+			if ( !targetObject.ContainsKey( propertyName ) ) return false; // Short-circuit return
+			targetObject = targetObject[ propertyName ]?.AsObject() ?? throw new JsonPropertyNullException( $"Nested JSON object property '{ propertyName }' in '{ propertyPath }' is null, it cannot be used as the next object" );
 		}
 
-		// Success if we got here
-		return true;
+		// Return if the property exists in the found object
+		return targetObject.ContainsKey( propertyNames.Last() );
 
 	}
 
-	// Gets a list of nested property paths in the JSON object
-	public static string[] NestedList( this JsonObject jsonObject ) {
-		return ListNestedProperties( jsonObject ).ToArray();
-	}
+	/// <summary>Gets a list of nested property paths within the JSON object .</summary>
+	/// <param name="jsonObject">The JSON object.</param>
+	/// <param name="propertyDelimiter">The character used to separate nested property names.</param>
+	/// <returns>An array of nested property paths.</returns>
+	public static string[] NestedList( this JsonObject jsonObject, char propertyDelimiter = Defaults.NestedPropertyDelimiter, string currentPath = "" ) {
 
-	// Recursively creates a list of nested property paths in the JSON object
-	private static List<string> ListNestedProperties( JsonObject jsonObject, string currentPath = "", List<string>? propertyPaths = null ) {
+		// Create an empty list for the property paths
+		List<string> propertyPaths = new();
 
-		// Create an empty list to hold the property paths if it is not set
-		propertyPaths ??= new();
-
-		// Loop through each valid property in the object...
+		// Loop through each property in the JSON object and create its path
 		foreach ( KeyValuePair<string, JsonNode?> property in jsonObject ) {
-			
-			// Create the path to this property by using the last call's path and this property name
-			string propertyPath = currentPath + property.Key;
+			string propertyPath = string.IsNullOrWhiteSpace( currentPath ) ? property.Key : currentPath + propertyDelimiter + property.Key;
 
-			// If the value is null then it must be a value so add it to the list
-			if ( property.Value == null ) {
-				propertyPaths.Add( propertyPath );
-				continue;
-			}
-
-			// Attempt to repeat this call with the value as a JSON object, otherwise add it to the list as it must be a value
+			// Recurse if the property is an object, otherwise add the path to the list
 			try {
-				ListNestedProperties( property.Value.AsObject(), propertyPath + Defaults.NestedPropertyDelimiter, propertyPaths );
+				JsonObject? nestedObject = property.Value?.AsObject();
+				if ( nestedObject != null ) propertyPaths.AddRange( nestedObject.NestedList( propertyDelimiter, propertyPath ) );
+				else propertyPaths.Add( propertyPath );
 			} catch ( InvalidOperationException ) {
 				propertyPaths.Add( propertyPath );
 			}
-
 		}
 
-		// Return the list
-		return propertyPaths;
+		// Convert the list to an array then return it
+		return propertyPaths.ToArray();
 
 	}
 
